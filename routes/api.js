28a1cab5a8695dc9b6dd33244b6d5cb3e99952f4
Jason <jrosasrjf@gmail.com>
2021-07-16 00:08:23 -0500

    added content to the api file

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,32 @@ const Transaction = require("../models/transactionSchema.js");
 
 
 /* -- updates to database -- */
-router.post(); 
+router.post("/api/transaction", ({ body }, res) => {
+    Transaction.create(body) // creating a new body using the Transaction schema 
+    .then(dbTransaction => { // issuing a promise here 
+        res.json(dbTransaction);    // displays that body as a json format
+    })
+    .catch(err => { // if we encounter an error, it would be a missing page. 
+        res.status(404).json(err);
+    });
+
+}); 
+
+
+
+
+
+/* -- Receive information from the database -- */
+router.get("/api/transaction", (req, res) => {
+    Transaction.find({ }).sort({date: -1})
+    .then(dbTransaction => { // issuing a promise here 
+        res.json(dbTransaction);    // displays that body as a json format
+    })
+    .catch(err => { // if we encounter an error, it would be a missing page. 
+        res.status(404).json(err);
+    });
+
+});
 
 
 
