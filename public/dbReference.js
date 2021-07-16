ee78d7088a32a572436a1673fb20cb184b782189
Jason <jrosasrjf@gmail.com>
2021-07-16 18:57:43 -0500

    added functions to the db file

--- public/dbReference.js
+++ public/dbReference.js
@@ -1,6 +1,7 @@
 let db; 
 const request = window.indexedDB.open("budget", 1); 
 
+
 /* -- functions -- */
 request.onsuccess = function (event) {
     db = event.target.result;
@@ -8,3 +9,34 @@ request.onsuccess = function (event) {
       checkDatabase();
     }
   };
+
+
+  request.onupgradeneeded = function(event) { // onupgradeneeded is an event handler when the database version is increased. 
+    const db = event.target.result; // defining the event 
+    db.createObjectStore("budgetStore", {
+      autoIncrement: true
+    }); 
+    budgetStore.createIndex("budgetIndex", "budgetIndex");
+  };
+
+
+  request.onerror = function (event) {
+    const msg = event.target.result
+    console.log(msg.errorCode)
+  };
+
+
+  function saveRecord(record) {
+    db = request.result;
+    const transaction = db.transaction(["budgetStore"], "readwrite");
+    const budgetStore = transaction.objectStore("budgetStore");
+
+    budgetStore.add(record);
+  };
+
+
+
+
+
+
+
