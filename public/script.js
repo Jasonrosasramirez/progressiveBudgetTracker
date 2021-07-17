807f3f6e4ef028b97e49d2f846a057cc250dcbca
Jason <jrosasrjf@gmail.com>
2021-07-16 19:30:43 -0500

    added in basic layout to functions

---
+++ public/script.js
@@ -0,0 +1,32 @@
+/* -- Variable Definitions -- */
+let transactionArray = []; // to be populated every instance. 
+let myChart;
+
+
+/* -- Define Functions -- */
+function populateThatTotal() {
+
+}
+
+
+function populateThatChart () {
+
+
+}
+
+
+function populateThatTable() {
+
+}
+
+
+/* -- Initiate Actions -- */
+fetch("/api/transaction")
+
+
+
+
+
+
+
+
