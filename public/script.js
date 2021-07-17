14d6b115e6b8cd6b0ec2e8d26ff0928861338aa7
Jason <jrosasrjf@gmail.com>
2021-07-16 20:49:35 -0500

    added in the populate table and total functions

--- public/script.js
+++ public/script.js
@@ -5,7 +5,13 @@ let myChart;
 
 /* -- Define Functions -- */
 function populateThatTotal() {
+    //.reduce .reduce(total, num) 
+  let totalTransaction = transactions.reduce((total, num) => { // executes reducer function on each element of the array resulting in a singl output
+    return total + parseInt(num.value);
+  }, 0);
 
+  let totalEl = document.querySelector("#total"); // applied to the html
+  totalEl.textContent = totalTransaction;
 }
 
 
@@ -16,13 +22,33 @@ function populateThatChart () {
 
 
 function populateThatTable() {
+    let tableBody = document.querySelector("#tbody");
+        tableBody.innerHTML = "";
 
+    transactions.forEach(transaction => {
+        let tableRow = document.createElement("tr");
+        tr.innerHTML = `
+        <td>${transaction.name}</td>
+        <td>${transaction.value}</td>
+        `;
+
+        tableBody.appendChild(tableRow);
+    });
 }
 
 
 /* -- Initiate Actions -- */
 fetch("/api/transaction")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        transactions = data;
 
+        populateThatTotal();
+        populateThatChart();
+        populateThatTable();
+    });
 
 
 
